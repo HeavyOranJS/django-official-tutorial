1ed90dd19a4154786403395c8b21972fbdf8f9f9
HeavyJS <whereorange@gmail.com>
2019-07-04 20:47:23 +1000

    lesson3_simpleViews-HTMLtemplates-404-CSS

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    #just checking what will happen
-    path('testo', views.index, name = 'testo')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/comments/', views.comment, name='comments')
 ]
