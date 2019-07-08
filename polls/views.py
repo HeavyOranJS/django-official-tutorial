8e574300766fef656b5199f9bdd1f69c4d3df035
HeavyJS <whereorange@gmail.com>
2019-07-08 15:21:59 +1000

    nonLesson_hidPagesWithNoInfo-hidLinksToSuchPages

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.db.models import Count
 
 from .models import Question, Choice, Comment
 
@@ -15,22 +16,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the las five published questions."""
+        """
+        Returns the last five published questions.
+        """
         now = timezone.now()
         return Question.objects.filter(pub_date__lte=now).order_by('-pub_date')[:5]
-
-# class DetailView(generic.DetailView):
-#     #model name, duh
-#     model = Question
-#     #override default template name <app name>/<model name>_detail.html
-#     #default name: polls/question_detail.html
-#     template_name = 'polls/detail.html'
-#     def qet_queryset(self):
-#         """
-#         Excludes not published questions. 
-#         """
-#         now = timezone.now()
-#         return Question.objects.filter(pub_date__lte=now)
 
 class DetailView(generic.DetailView):
     #model name, duh
@@ -40,7 +30,7 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     def get_queryset(self):
         """
-        Excludes any questions that aren't published yet.
+        Returns questions with publishing date older than now.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
@@ -48,9 +38,21 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+    def get_queryset(self):
+        """
+        Returns all questions with at least 1 choice.
+        """
+        return Question.objects.annotate(num_choice=Count('choice')).filter(num_choice__gt=0)
+
 class CommentsView(generic.DetailView):
     model = Question
     template_name = 'polls/comments.html'
+
+    def get_queryset(self):
+        """
+        Returns all questions with at least 1 comment.
+        """
+        return Question.objects.annotate(num_comment=Count('comment')).filter(num_comment__gt=0)
 
 def leave_comment(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
