71e25710c2c02ab61a4629494c43d5d5f4beec64
HeavyJS <whereorange@gmail.com>
2019-07-09 17:30:05 +1000

    lesson7_adminDashboard-templates

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
 from .models import Question, Choice, Comment
 
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Comment)
-# Register your models here.
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {
+            'fields': ['pub_date']
+            }
+        )
+    ]
+    inlines = [ChoiceInLine, CommentInLine]
+    #NOTE: calculated fields are not sortable. There are tricks to make
+    #it work by overriding get_queryset tho
+    list_display = (
+        'question_text',
+        'pub_date',
+        'was_published_recently',
+        'count_comments',
+        'count_comments_positive',
+        'count_comments_negative'
+        )
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
--- polls/models.py
+++ polls/models.py
@@ -13,10 +13,39 @@ class Question(models.Model):
         return "{}".format(
             self.question_text)
 
+    def count_comments(self):
+        """
+        Returns amount of comments
+        """
+        this_question = Question.objects.get(pk=self.pk)
+        return this_question.comment_set.all().count()
+
+    def count_comments_positive(self):
+        """
+        Returns amount of positive comments
+        """
+        this_question = Question.objects.get(pk=self.pk)
+        return this_question.comment_set.filter(positive=True).count()
+
+    def count_comments_negative(self):
+        """
+        Returns amount of negative comments
+        """
+        this_question = Question.objects.get(pk=self.pk)
+        return this_question.comment_set.filter(positive=False).count()
+
     def was_published_recently(self):
         """Determins if question was published less than day ago"""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    #admin dashboard options
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    count_comments.short_description = "Comments"
+    count_comments_positive.short_description = "Postive"
+    count_comments_negative.short_description = "Negative"
 
 class Comment(models.Model):
     """Comments to question. Contains link to question, comment text and bool positive"""
@@ -29,6 +58,10 @@ class Comment(models.Model):
             "Positive" if self.positive else "Negative",
             self.question,
             self.comment_text)
+
+    #admin dashboard options
+    positive.boolean = True
+    positive.short_description = 'Comment raiting'
 
 class Choice(models.Model):
     """Vote options to question. Contains link to question, option text and amount of votes"""
